1b0ac2ee43586565bb17561cce34f812b1c48353
SapunArtem <sapun_artem@mail.ru>
2022-02-14 19:48:46 +0300

    Bean Lifecycle,Init,Destrpy,Factory methods

--- src/main/java/by/sapun/spring/MusicPlayer.java
+++ src/main/java/by/sapun/spring/MusicPlayer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MusicPlayer {
-    private List<Music> musicList=new ArrayList<>();
+    private Music music;
     private String name;
     private int volume;
 
@@ -23,13 +23,16 @@ public class MusicPlayer {
     public void setVolume(int volume) {
         this.volume = volume;
     }
+//IoC
+   public MusicPlayer(Music music){
+        this.music = music;
+   }
+   public MusicPlayer(){}
+    public void setMusic(Music music){
+        this.music = music;
+    }
+    public void playMusic(){
+        System.out.println("Plaing:"+music.getSong());
+    }
 
-    public void setMusicList(List<Music> musicList){
-        this.musicList=musicList;
-    }
-    public void playMusicList(){
-        for (Music music:musicList) {
-            System.out.println("playing: " + music.getSong());
-        }
-    }
 }
--- src/main/java/by/sapun/spring/TestSpring.java
+++ src/main/java/by/sapun/spring/TestSpring.java
@@ -6,12 +6,25 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        //Music music = context.getBean("MusicBean",Music.class);
-       // MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-        musicPlayer.playMusicList();
+        ClassicalMusic classicalMusic = context.getBean("musicBean",ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+       /* MusicPlayer firstPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer secondPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+
+        boolean comparison = firstPlayer == secondPlayer;
+
+        System.out.println(comparison);
+        System.out.println(firstPlayer);
+        System.out.println(secondPlayer);
+
+        firstPlayer.setVolume(10);
+        System.out.println(firstPlayer.getVolume());
+        System.out.println(secondPlayer.getVolume());*/
+
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
+
         context.close();
     }
 }
