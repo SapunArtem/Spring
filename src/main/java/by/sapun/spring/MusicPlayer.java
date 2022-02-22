0817373ef5b764999a860459b1ba88797f41f1e4
SapunArtem <sapun_artem@mail.ru>
2022-02-22 20:02:58 +0300

    Annotation @Qualifier

--- src/main/java/by/sapun/spring/MusicPlayer.java
+++ src/main/java/by/sapun/spring/MusicPlayer.java
@@ -1,6 +1,7 @@
 package by.sapun.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,17 +9,18 @@ import java.util.List;
 @Component
 public class MusicPlayer {
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private  Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic(){
-        return "Plaing:"+classicalMusic.getSong();
+        return "Plaing:"+music1.getSong() + "," + music2.getSong();
 
     }
 
